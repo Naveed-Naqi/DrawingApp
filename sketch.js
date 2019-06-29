147ce828be95ff42a48d48fd6b77f6e342e22020
Naveed Naqi <nnaqi5534@gmail.com>
2019-06-29 16:09:39 -0400

    update sketch

--- sketch.js
+++ sketch.js
@@ -1,1 +1,13 @@
 /// <reference path="p5.d/p5.global-mode.d.ts"/>
+
+function setup()
+{
+    createCanvas(windowWidth, windowHeight);
+    background(220);
+}
+
+function mouseDragged()
+{
+    noStroke();
+    ellipse(mouseX, mouseY, 50, 50);
+}
